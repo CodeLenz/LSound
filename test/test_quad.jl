356549e0375bb94977a8482a2a8cb408be5aa78e
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-09-09 13:21:40 -0300

    Update tests for dNrs_bi4

--- test/test_quad.jl
+++ test/test_quad.jl
@@ -10,8 +10,24 @@
 
     # Test dN
     dNr,dNs = LSound.dNrs_bi4(-1,-1)
-    @test all(dNr .== [-2;2;0;0])
-    @test all(dNs .== [-2;0;0;2])
+    @test all(dNr .== [-0.5;0.5;0;0])
+    @test all(dNs .== [-0.5;0;0;0.5])
+
+    dNr,dNs = LSound.dNrs_bi4(1,-1)
+    @test all(dNr .== [-0.5;0.5;0;0])
+    @test all(dNs .== [ 0;-0.5;0.5;0])
+
+    dNr,dNs = LSound.dNrs_bi4(1,1)
+    @test all(dNr .== [0;0;0.5;-0.5])
+    @test all(dNs .== [ 0;-0.5;0.5;0])
+    
+    dNr,dNs = LSound.dNrs_bi4(-1,1)
+    @test all(dNr .== [0;0;0.5;-0.5])
+    @test all(dNs .== [ -0.5;0;0;0.5])
+
+    dNr,dNs = LSound.dNrs_bi4(0,0)
+    @test all(dNr .== [-0.25; 0.25; 0.25; -0.25])
+    @test all(dNs .== [-0.25; -0.25; 0.25; 0.25])
 
 
 end
